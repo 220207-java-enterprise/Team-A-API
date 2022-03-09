e57f3c4b83b60d55a34d322418d04a2c5777c569
Gmander <aidanfire360@gmail.com>
2022-03-09 17:27:04 -0500

    project structuring

--- src/main/java/com/revature/foundation/Driver.java
+++ src/main/java/com/revature/foundation/Driver.java
@@ -1,4 +1,4 @@
-package com.revature.boot;
+package com.revature.foundation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
