9e59f710b4f8423386ee632da93b60ce719594ef
Gmander <aidanfire360@gmail.com>
2022-03-10 16:22:48 -0500

    Spring Done. Lego

--- src/main/java/com/revature/foundation/repository/UsersRepository.java
+++ src/main/java/com/revature/foundation/repository/UsersRepository.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersRepository extends CrudRepository<Users, String> {
-    @Query("from ers_users where ers_users.user_id = ?1")
+
+    @Query("from Users u where u.userId = ?1") // JPQL/HQL
+//    @Query(value = "select * from ers_users where ers_users.user_id = ?1", nativeQuery = true)
     Users getUserById(String user_id);
 
 
