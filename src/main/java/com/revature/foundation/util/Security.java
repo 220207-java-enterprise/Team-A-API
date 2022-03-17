f10c890ff413ee40c0096e24546e26d5db8ca5b8
= <=>
2022-03-16 17:36:18 -0700

    Added password confirmation

--- src/main/java/com/revature/foundation/util/Security.java
+++ src/main/java/com/revature/foundation/util/Security.java
@@ -8,8 +8,7 @@ import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 
 public class Security {
-
-
+    // just to test git
             public static String getEncryptedPassword(String originalPassword)
                 throws NoSuchAlgorithmException, InvalidKeySpecException {
             // String originalPassword = "password";
@@ -19,6 +18,28 @@ public class Security {
             // System.out.println(generatedSecuredPasswordHash);
             return generatedSecuredPasswordHash;
             }
+
+    private static boolean validatePassword(String originalPassword, String storedPassword)
+            throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        String[] parts = storedPassword.split(":");
+        int iterations = Integer.parseInt(parts[0]);
+
+        byte[] salt = fromHex(parts[1]);
+        byte[] hash = fromHex(parts[2]);
+
+        PBEKeySpec spec = new PBEKeySpec(originalPassword.toCharArray(),
+                salt, iterations, hash.length * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] testHash = skf.generateSecret(spec).getEncoded();
+
+        int diff = hash.length ^ testHash.length;
+        for(int i = 0; i < hash.length && i < testHash.length; i++)
+        {
+            diff |= hash[i] ^ testHash[i];
+        }
+        return diff == 0;
+    }
 
         private static String generateStrongPasswordHash(String password)
                 throws NoSuchAlgorithmException, InvalidKeySpecException {
@@ -52,6 +73,29 @@ public class Security {
             }
         }
 
+//    String  originalPassword = "password";
+//
+//    String generatedSecuredPasswordHash
+//            = generateStorngPasswordHash(originalPassword);
+//    System.out.println(generatedSecuredPasswordHash);
+//
+//    boolean matched = validatePassword("password", generatedSecuredPasswordHash);
+//    System.out.println(matched);
+//
+//    matched = validatePassword("password1", generatedSecuredPasswordHash);
+//    System.out.println(matched);
+//    }
 
 
+    private static byte[] fromHex(String hex) throws NoSuchAlgorithmException
+    {
+        byte[] bytes = new byte[hex.length() / 2];
+        for(int i = 0; i < bytes.length ;i++)
+        {
+            bytes[i] = (byte)Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        }
+        return bytes;
     }
+
+
+}
