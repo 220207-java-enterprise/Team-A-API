65965a629b0f08bc770280b80a45fd276ced9561
= <=>
2022-03-17 13:44:41 -0700

    Another hoop to jump through, switching contexts.

--- src/main/java/com/revature/foundation/util/Security.java
+++ src/main/java/com/revature/foundation/util/Security.java
@@ -1,23 +1,23 @@
 package com.revature.foundation.util;
 
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import java.math.BigInteger;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
+        import javax.crypto.SecretKeyFactory;
+        import javax.crypto.spec.PBEKeySpec;
+        import java.math.BigInteger;
+        import java.security.NoSuchAlgorithmException;
+        import java.security.SecureRandom;
+        import java.security.spec.InvalidKeySpecException;
 
 public class Security {
     // just to test git
-            public static String getEncryptedPassword(String originalPassword)
-                throws NoSuchAlgorithmException, InvalidKeySpecException {
-            // String originalPassword = "password";
+    public static String getEncryptedPassword(String originalPassword)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        // String originalPassword = "password";
 
-            String generatedSecuredPasswordHash
-                    = generateStrongPasswordHash(originalPassword);
-            // System.out.println(generatedSecuredPasswordHash);
-            return generatedSecuredPasswordHash;
-            }
+        String generatedSecuredPasswordHash
+                = generateStrongPasswordHash(originalPassword);
+        // System.out.println(generatedSecuredPasswordHash);
+        return generatedSecuredPasswordHash;
+    }
 
     private static boolean validatePassword(String originalPassword, String storedPassword)
             throws NoSuchAlgorithmException, InvalidKeySpecException
@@ -41,37 +41,37 @@ public class Security {
         return diff == 0;
     }
 
-        private static String generateStrongPasswordHash(String password)
-                throws NoSuchAlgorithmException, InvalidKeySpecException {
-            int iterations = 1000;
-            char[] chars = password.toCharArray();
-            byte[] salt = getSalt();
+    private static String generateStrongPasswordHash(String password)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        int iterations = 1000;
+        char[] chars = password.toCharArray();
+        byte[] salt = getSalt();
 
-            PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
-            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
 
-            byte[] hash = skf.generateSecret(spec).getEncoded();
-            return iterations + ":" + toHex(salt) + ":" + toHex(hash);
+        byte[] hash = skf.generateSecret(spec).getEncoded();
+        return iterations + ":" + toHex(salt) + ":" + toHex(hash);
+    }
+
+    private static byte[] getSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
+    }
+
+    private static String toHex(byte[] array) throws NoSuchAlgorithmException {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+
+        int paddingLength = (array.length * 2) - hex.length();
+        if (paddingLength > 0) {
+            return String.format("%0" + paddingLength + "d", 0) + hex;
+        } else {
+            return hex;
         }
-
-        private static byte[] getSalt() throws NoSuchAlgorithmException {
-            SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-            byte[] salt = new byte[16];
-            sr.nextBytes(salt);
-            return salt;
-        }
-
-        private static String toHex(byte[] array) throws NoSuchAlgorithmException {
-            BigInteger bi = new BigInteger(1, array);
-            String hex = bi.toString(16);
-
-            int paddingLength = (array.length * 2) - hex.length();
-            if (paddingLength > 0) {
-                return String.format("%0" + paddingLength + "d", 0) + hex;
-            } else {
-                return hex;
-            }
-        }
+    }
 
 //    String  originalPassword = "password";
 //
