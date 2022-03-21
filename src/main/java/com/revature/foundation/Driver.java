506a6bdbd9e5ab463e2dfb326aa0c39ae8305eda
GuayaRainbow <amelia124@revature.net>
2022-03-20 16:28:12 -1000

    fixed pom for logging, implemented logging to console, need to check logging to file.

--- src/main/java/com/revature/foundation/Driver.java
+++ src/main/java/com/revature/foundation/Driver.java
@@ -1,16 +1,23 @@
 package com.revature.foundation;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication // implies both @Configuration and @ComponentScan com.revature.foundation.services
 //@ComponentScan("com.revature.foundation.services")
 public class Driver {
 
+    private static Logger logger = LogManager.getLogger(Driver.class);
     public static void main(String[] args) {
 
-        SpringApplication.run(Driver.class);
+        logger.debug("Debug message");
+        logger.info("Info message");
+        logger.warn("Warn message");
+        logger.error("Error message");
+        logger.fatal("Fatal message");
+        SpringApplication.run(Driver.class, args);
     }
 
 }
